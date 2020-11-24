39a6a8b995a746b29a4d44b5f8cacf020b104ce3
Alican Çelik <alicancelikcan@gmail.com>
2020-11-24 10:32:54 +0300

    YMS 5174 Deneme

---
+++ Core_TagHelpers/Core_TagHelpers/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core_TagHelpers.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_TagHelpers.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public IActionResult HtmlHelperSample()
+        //{
+        //    return View();
+        //}
+        public IActionResult HtmlHelperSample() => View(); //Üsttekiyle aynı
+
+        [HttpPost]
+        public IActionResult HtmlHelperSample(User u)
+        {
+            return View();
+        }
+
+        public IActionResult TagHelperSample() => View();
+
+        [HttpPost]
+        public IActionResult TagHelperSample(User u)
+        {
+            return View();
+        }
+
+    }
+}
